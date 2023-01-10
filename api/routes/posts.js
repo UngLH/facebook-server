bb3d90ecff7e6e5b86682fd73dd5229224866e61
UngLH <lehongung.work@gmail.com>
2023-01-10 14:33:50 +0700

    add google cloud upload

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -379,8 +379,7 @@ function uploadFile(file) {
             contentType: file.mimetype,
         },
     });
-    const publicUrl =
-        `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURI(blob.name)}?alt=media`;
+    const publicUrl =`https://storage.googleapis.com/${bucket.name}/${encodeURI(blob.name)}`;
     return new Promise((resolve, reject) => {
 
         blobStream.on('error', function(err) {
